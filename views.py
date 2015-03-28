27aaf2912b67ed3b90f1606119f8b38bc35004e7
katstevens <kat.stevens@gmail.com>
2015-03-28 11:53:08 +0000

    Create views.py
    View for Page model objects and Home.

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+from mybrilliantproject.pages.models import Page
+from mybrilliantproject.utils.globalfunctions import get_cookie_acceptance
+
+def basic_page(request, slug=False):
+	"Show Page model info in basic template"
+
+	page = get_object_or_404(Page, slug=slug,visible=True)
+
+	if page.redirect_url:
+		return redirect(page.redirect_url)
+	
+	if slug == "recommendations":
+		template = "recs.html"
+	else:
+		template = "base.html"
+		
+	return render(request, template, {
+			'page':page,
+			'accept_cookies':get_cookie_acceptance(request),	# Display cookie message
+		})
+		
+def home(request):
+	"Named view for Home page for easy URL reference in templates"
+	return basic_page(request, slug='home')
