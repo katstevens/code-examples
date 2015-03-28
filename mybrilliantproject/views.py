e09de5faf3b84bc8347b6b1b6fd3bb00ba15f876
katstevens <kat.stevens@gmail.com>
2015-03-28 12:17:49 +0000

    Update views.py

--- mybrilliantproject/views.py
+++ mybrilliantproject/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from mybrilliantproject.pages.models import Page
-from mybrilliantproject.utils.globalfunctions import get_cookie_acceptance
+from mybrilliantproject.models import Page
+from mybrilliantproject.globalfunctions import get_cookie_acceptance
 
 def basic_page(request, slug=False):
 	"Show Page model info in basic template"
@@ -11,7 +11,7 @@ def basic_page(request, slug=False):
 	if page.redirect_url:
 		return redirect(page.redirect_url)
 	
-	if slug == "recommendations":
+	if slug == "recommendations":		# Handle custom templates for different pages
 		template = "recs.html"
 	else:
 		template = "base.html"
