d6997b078144f5c4661b1cbf95934e21f2b4e693
katstevens <kat.stevens@gmail.com>
2015-03-28 12:10:12 +0000

    Create __init__.py

---
+++ mybrilliantproject/__init__.py
@@ -0,0 +1,5 @@
+"""
+In lieu of anything exciting going on, here I'd normally put a brief rundown of 
+everything that's going on in the folder for my own reference, for example, any 
+additional modules like 'wacky_specific_functions.py'.
+"""
